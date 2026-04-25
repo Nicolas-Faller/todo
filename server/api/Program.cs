09eeaaa6e52a2305dc04e431feeca64049da3114
Nicolas-Faller <nfaller.rios@gmail.com>
2026-04-25 07:19:02 -0300

    Deployment

--- server/api/Program.cs
+++ server/api/Program.cs
@@ -1,19 +1,29 @@
+using System.Text.Json;
+using api;
 using efscaffold.Entities;
 using Infrastructure.Postgres.Scaffolding;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
 
 var builder = WebApplication.CreateBuilder(args);
 
+var appOptions = builder.Services.AddAppOptions(builder.Configuration);
+
+Console.WriteLine(JsonSerializer.Serialize(appOptions));
+
 builder.Services.AddDbContext<MyDbContext>(conf =>
     {
-      conf.UseNpgsql("Server=ep-shy-union-anj1v6zt-pooler.c-6.us-east-1.aws.neon.tech;DB=neondb;UID=neondb_owner;PWD=npg_v1zFMxIbG8ao;SslMode=require");
+      conf.UseNpgsql(appOptions.DbConnectionString);
     });
 
 
 var app = builder.Build();
 
-app.MapGet("/", ([FromServices] MyDbContext dbContext) =>
+app.MapGet("/", (
+
+      [FromServices] IOptionsMonitor<AppOptions> optionsMonitor,
+      [FromServices] MyDbContext dbContext) =>
     {
       var myTodo = new Todo()
       {
